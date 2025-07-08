ab86dd98d0310717e5f909eb052189ef05f47086
Bhukya Mourya Vardhan Rathod <mouryabhukya04@gmail.com>
2025-07-08 11:30:16 +0530

    file updated with key

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
- 
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Mourya Vardhan',
+    author_email='mouryabhukya04@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
